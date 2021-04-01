eb8af7c15bab0003605baf9f78c3b745217bae83
Kenji Brameld <kenjibrameld@gmail.com>
2021-04-01 18:51:50 +0900

    remove print statement used for debugging

--- app/Capture.py
+++ app/Capture.py
@@ -85,8 +85,6 @@ class Capture:
         start_time = time.time()
         while time.time() - start_time < capture_seconds:
 
-            print(time.time() - start_time)
-
             time_before = time.time()
 
             successful, frame = self._cap.read()
