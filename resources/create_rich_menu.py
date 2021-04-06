294179fbab5f989282a83a62c95596bd952eb486
Kenji Brameld <kenjibrameld@gmail.com>
2021-04-06 15:26:05 +0900

    update rich menu code

--- resources/create_rich_menu.py
+++ resources/create_rich_menu.py
@@ -9,15 +9,17 @@ twitch_username = os.environ['TWITCH_USERNAME']
 
 line_bot_api = LineBotApi(channel_access_token)
 
+display_text = "録画始まりました、どうぞ滑ってください！"
+
 rich_menu_to_create = RichMenu(
-    size=RichMenuSize(width=2500, height=1408),
+    size=RichMenuSize(width=2500, height=1162),
     selected=False,
     name="Nice richmenu",
     chat_bar_text="録画ボタン",
     areas=[RichMenuArea(
         bounds=RichMenuBounds(x=0, y=0, width=2500, height=1408),
-        action=PostbackAction(data=twitch_username, 
-                              display_text="録画スタートをリクエストしました"))]
+        action=PostbackAction(data=twitch_username,
+                              display_text=display_text))]
 )
 rich_menu_id = line_bot_api.create_rich_menu(rich_menu=rich_menu_to_create)
 
